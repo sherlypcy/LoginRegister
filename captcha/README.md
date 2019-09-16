4132c401b8b4a737d78511317de5ff0dae9091c4
sherlypcy <46549449+sherlypcy@users.noreply.github.com>
2019-09-16 20:04:18 +0700

    login

---
+++ captcha/README.md
@@ -0,0 +1,14 @@
+# H171600554_Muhammad-Rizky-Rizaldi_WEB2
+Tugas Pemrograman Web II
+
+# Changelog
+Commit 5 September 2019
+- 06:27 #9ce4e01: Tugas 1 diupload (Login & Register)
+
+Commit 11 September 2019
+- 18:43 #003579b: Test Login
+- 21:38 #a5dc33f: Login/Register dengan No. Telp berhasil
+- 23:42 #e7484dd: Captcha pada Login & Register selesai
+
+Commit 12 September 2019
+- 07:00 #dc13afa: Tugas2 Login & Register sudah fixed
---
+++ captcha/routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::any('captcha-test', function() {
+    if (request()->getMethod() == 'POST') {
+        $rules = ['captcha' => 'required|captcha'];
+        $validator = validator()->make(request()->all(), $rules);
+        if ($validator->fails()) {
+            echo '<p style="color: #ff0000;">Incorrect!</p>';
+        } else {
+            echo '<p style="color: #00ff30;">Matched :)</p>';
+        }
+    }
+
+    $form = '<form method="post" action="captcha-test">';
+    $form .= '<input type="hidden" name="_token" value="' . csrf_token() . '">';
+    $form .= '<p>' . captcha_img() . '</p>';
+    $form .= '<p><input type="text" name="captcha"></p>';
+    $form .= '<p><button type="submit" name="check">Check</button></p>';
+    $form .= '</form>';
+    return $form;
+});
